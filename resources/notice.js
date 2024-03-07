44c01e6f1ac86da12fc1534cf940b41f4e2071d5
G0RD4LH4 <gordalha77@gmail.com>
2024-03-07 09:30:16 -0300

    Correção na formatação de data para enviar alertas

--- resources/notice.js
+++ resources/notice.js
@@ -12,11 +12,21 @@ const transporter = nodemailer.createTransport({
 })
 
 function formatDate(data) {
+    if (!(data instanceof Date)) {
+        data = new Date(data)
+    }
+
+    if (isNaN(data.getTime())) {
+        return 'Data inválida'
+    }
+
     const dia = data.getDate() < 10 ? '0' + data.getDate() : data.getDate()
     const mes = (data.getMonth() + 1) < 10 ? '0' + (data.getMonth() + 1) : data.getMonth() + 1
     const ano = data.getFullYear()
+
     return `${dia}/${mes}/${ano}`
 }
+
 
 function formatDateForUpdate(input) {
     if (!input) return null
