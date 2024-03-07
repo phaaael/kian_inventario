a90296dec3d23081c2a450fe8daf830bbf176cfb
G0RD4LH4 <gordalha77@gmail.com>
2024-03-07 12:15:06 -0300

    Alterado alertas na área de registro de usuário

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -9,15 +9,17 @@ const getRegister = (req, res) => {
 const register = async (req, res) => {
     const { username, password, password_validation, name, surname } = req.body
     try {
-        if (password !== password_validation) return res.send('<script>alert("As senhas não coincidem"); window.location.href = "/register";</script>')
+        if (password !== password_validation) {
+            return res.json({ success: false, message: "As senhas não coincidem" })
+        }
 
         const insert = 'INSERT INTO kian_usuarios(usuario, senha, nome, sobrenome) VALUES(?, ?, ?, ?)'
         await registerDatabase.pool.execute(insert, [username, password, name, surname])
 
-        res.send('<script>alert("Credenciais registradas"); window.location.href = "/";</script>')
+        res.json({ success: true })
     } catch (error) {
         res.status(500).send('Erro interno ao enviar credenciais')
     }
-};
+}
 
 module.exports = { getRegister, register }
---
+++ views/scripts/register.js
@@ -0,0 +1,50 @@
+document.querySelector('.register').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    let formData = new FormData(this)
+    let object = {}
+    formData.forEach((value, key) => { object[key] = value; })
+    let json = JSON.stringify(object)
+
+    fetch('/register/submit', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: json
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Houve um problema com a resposta da rede')
+        }
+        return response.json()
+    })
+    .then(data => {
+        if (data.success) {
+            swal({
+                title: "Sucesso",
+                text: "Registro Concluído com Sucesso",
+                icon: "success",
+                button: "OK",
+            }).then((value) => {
+                window.location.href = '/'
+            })
+        } else {
+            swal({
+                title: "Erro",
+                text: data.message || "Não foi possível concluir o registro",
+                icon: "error",
+                button: "OK",
+            })
+        }
+    })
+    .catch(error => {
+        swal({
+            title: "Erro",
+            text: "Não foi possível concluir a solicitação",
+            icon: "error",
+            button: "OK",
+        })
+    })
+})
