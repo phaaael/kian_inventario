60cbe3e3ef601ac3dca53abf44e9649cdcc546e0
G0RD4LH4 <gordalha77@gmail.com>
2024-03-13 14:06:46 -0300

    Implementado uma função para cada alerta

--- views/scripts/allrequests.js
+++ views/scripts/allrequests.js
@@ -1,3 +1,7 @@
-function showAlert(message) {
+function alertReason(message) {
     swal("Motivo da Solicitação", message)
+}
+
+function alertStatus(message) {
+    swal("Motivo da Recusa", message)
 }
