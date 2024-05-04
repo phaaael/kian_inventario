77bb92c53b155bb2684e8a59a8fb8db846b4dcf5
phaaael <gordalha77@gmail.com>
2024-05-04 00:17:57 -0300

    Implementado rodapé para exibir a versão da aplicação

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,7 @@
 const getHome = (req, res) => {
     const dynamicData = { 
         title: 'Kian Inventário',
-        desc: 'Solicitações de Equipamentos ao TI'
+        desc: 'Solicitações de Equipamentos/Suprimentos ao TI'
     }
 
     res.render('home', { data: dynamicData })
