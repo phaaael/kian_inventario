70c18326c0fb160d65b827a145218ce922ec0fbb
phaaael <gordalha77@gmail.com>
2024-05-04 23:36:28 -0300

    Descricao alterada

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,7 @@
 const getHome = (req, res) => {
     const dynamicData = { 
         title: 'Kian Inventário',
-        desc: 'Solicitações de Equipamentos/Suprimentos ao TI'
+        desc: 'Requerimentos de Equipamentos/Suprimentos ao TI'
     }
 
     res.render('main/home', { data: dynamicData })
